31b26005f83bd0aba8fe32b088aa41f2065d73f8
doodream <doodream17@naver.com>
2023-02-22 23:03:03 +0900

    :wrench: firebase config 환경변수 세팅

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   env: {
-    FIREBASE_API_KEY: 'AIzaSyAPPDPbRJpIyu85TfVohtElpSnt7rFehN0',
-    FIREBASE_AUTH_DOMAIN: 'chawchaw.firebaseapp.com',
-    FIREBASE_PROJECT_ID: 'chawchaw',
-    FIREBASE_STORAGE_BUCKET: 'chawchaw.appspot.com',
-    FIREBASE_MESSAGING_SENDER_ID: '259242347220',
-    FIREBASE_APP_ID: 'G-Y17VMGKWTH',
-    MEASUREMENT_ID: 'G-Y17VMGKWTH'
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    MEASUREMENT_ID: process.env.MEASUREMENT_ID
   },
   reactStrictMode: true,
   swcMinify: true,
