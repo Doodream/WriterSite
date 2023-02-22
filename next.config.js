1abf37cc20660d25156048283e844e54094f0d7b
doodream <doodream17@naver.com>
2023-02-22 15:08:20 +0900

    :heavy_plus_sign: firebase 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  env: {
+    FIREBASE_API_KEY: 'AIzaSyAPPDPbRJpIyu85TfVohtElpSnt7rFehN0',
+    FIREBASE_AUTH_DOMAIN: 'chawchaw.firebaseapp.com',
+    FIREBASE_PROJECT_ID: 'chawchaw',
+    FIREBASE_STORAGE_BUCKET: 'chawchaw.appspot.com',
+    FIREBASE_MESSAGING_SENDER_ID: '259242347220',
+    FIREBASE_APP_ID: 'G-Y17VMGKWTH',
+    MEASUREMENT_ID: 'G-Y17VMGKWTH'
+  },
   reactStrictMode: true,
   swcMinify: true,
   webpack(config) {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,22 @@ import type { AppProps } from 'next/app'
 import React from 'react'
 import '@/styles/globals.css'
 import Layout from '@/components/common/layout'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const app = initializeApp(firebaseConfig)
+  // const analytics = getAnalytics(app)
   return (
     <>
       <Layout>
